fdb1f63209df0c37014843d40f2993108d99884f
maximilianocuevas777 <maximilianocuevas777@gmail.com>
2019-04-21 11:52:59 -0700

    Add source code

---
+++ SourceCode/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f74f6ba2-85f0-4f10-afe3-07db0367fbe0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SourceCode/UnitTestProject/StringCreatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ExampleDLLProjectForUnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class StringCreatorTest
+    {
+        private StringCreator subject;
+
+        [TestInitialize]
+        public void setUp() {
+            subject = new StringCreator();
+        }
+
+        [TestMethod]
+        public void getStringToReturn()
+        {
+
+            string expected = "I expect this string to return";
+            string actual = subject.getStringToReturn(expected);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
