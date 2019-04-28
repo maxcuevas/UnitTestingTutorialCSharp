9450ba75693a7b7718deb5de718ffb17f4db99bf
Max Cuevas <maximilianocuevas777@gmail.com>
2019-04-28 10:30:27 -0700

    ADD folder with DLLs

---
+++ SourceCode/ExampleDLLUsingMoqForMockingUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExampleDLLUsingMoqForMockingUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExampleDLLUsingMoqForMockingUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc97d042-43ed-408c-b4a5-b9cca7512fad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
